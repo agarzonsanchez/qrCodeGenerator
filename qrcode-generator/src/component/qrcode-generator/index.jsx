4c5fdaf45f8d855d9763a9f0df167e53b51b8c88
Andres  Garzon <andresgarzon@andress-imac.home>
2024-08-26 19:12:53 -0400

    qr code app working. Styles OK

--- qrcode-generator/src/component/qrcode-generator/index.jsx
+++ qrcode-generator/src/component/qrcode-generator/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import QRCode from "react-qr-code";
+import "./index.css";
 
 export default function QrCodeGenerator() {
   // install qrcode generator npm i react-qr-code
@@ -9,25 +10,31 @@ export default function QrCodeGenerator() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e.target.value);
+    console.log(input);
     setQrCodeValue(input);
+    setInput("");
   }
   return (
     <div>
-      <div>
+      <div className="title-style">
         <h1>QR Code Generator</h1>
+        <p>Type a word, hit submit and it will generate a QR code.</p>
       </div>
 
       <div>
         <QRCode size={300} value={qrCodeValue} viewBox={`0 0 256 256`} />
       </div>
-      <div>
+      <div className="submit-button-div">
         <input
+          className="input-style"
           type="text"
           placeholder="Type a word"
           onChange={(e) => setInput(e.target.value)}
+          value={input}
         />
-        <button onClick={handleSubmit}>Submit</button>
+        <button className="button-style" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
     </div>
   );
